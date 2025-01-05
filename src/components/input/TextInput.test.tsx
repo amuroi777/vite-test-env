fb9c419e825879cb2aca3690b6740d9a6a8f2a42
amuroi777 <muroiaki7722@gmail.com>
2025-01-05 21:06:01 +0900

    lint-staged

--- src/components/input/TextInput.test.tsx
+++ src/components/input/TextInput.test.tsx
@@ -5,9 +5,16 @@ import userEvent from "@testing-library/user-event";
 test("textInput", async () => {
   const user = userEvent.setup();
   render(<TextInput />);
+
   const inputElement = screen.getByRole("textbox");
   expect(inputElement).toBeInTheDocument();
+});
 
-  await user.type(inputElement, "Hello world");
-  expect(screen.getByText("Hello world")).toBeInTheDocument();
+test("TextInput Event Test", async () => {
+  const user = userEvent.setup();
+  render(<TextInput />);
+
+  const inputElement = screen.getByRole("textbox");
+  await user.type(inputElement, "Hello World");
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
 });
